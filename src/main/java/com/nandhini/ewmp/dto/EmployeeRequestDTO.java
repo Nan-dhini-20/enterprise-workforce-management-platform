6f074b45aedd3d6c896eb653b0937c52092df982
Nandhini <nandhiniu206@gmail.com>
2026-05-30 18:05:16 +0530

    Added employee CRUD, validation and exception handling

---
+++ src/main/java/com/nandhini/ewmp/dto/EmployeeRequestDTO.java
@@ -0,0 +1,62 @@
+package com.nandhini.ewmp.dto;
+
+public class EmployeeRequestDTO {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String designation;
+    private Double salary;
+    private Long departmentId;
+
+    public EmployeeRequestDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+}
---
+++ src/main/java/com/nandhini/ewmp/dto/EmployeeResponseDTO.java
@@ -0,0 +1,71 @@
+package com.nandhini.ewmp.dto;
+
+public class EmployeeResponseDTO {
+
+    private Long employeeId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String designation;
+    private Double salary;
+    private String departmentName;
+
+    public EmployeeResponseDTO() {
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
